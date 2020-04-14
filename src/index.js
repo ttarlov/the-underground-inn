306bac51f0c9c7bb4b79e81b96648ff1f5e53004
Taras Tarlov <ttarlov@gmail.com>
2020-04-14 15:04:31 -0600

    add generateUser function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
+import User  from './user'
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+const generateUserId = () => {
+  let userName = $('#username-input').val();
+  let userId = userName.match(/\d+/g);
+  if(userId > 50) {
+    window.alert('user id does not exist, try again')
+    location.reload();
+  } else {
+    return Number(userId);
+    }
+}
+
+// will go inside a user class
+function processLogIn(data) {
+  if($('#password-input').val() === 'travel2020' && $('#username-input').val().includes('travel'))  {
+    event.preventDefault();
+    getTravelerData()
+    domUpdates.hideLoginWindow();
+  } else if ($('#password-input').val() === 'travel2020' && $('#username-input').val().includes('agency')) {
+    event.preventDefault();
+    getAgencyData();
+    domUpdates.hideLoginWindow();
+    // domUpdates.showWelcomeCard();
+  } else {
+    window.alert("Wrong Password or User Name")
+  }
+};
