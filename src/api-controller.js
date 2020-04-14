421fc2a63503b1d1ee663ddaf67048ee0468ad7c
Taras Tarlov <ttarlov@gmail.com>
2020-04-14 17:47:03 -0600

    add apiController Class and methods to fetch data

---
+++ src/api-controller.js
@@ -0,0 +1,24 @@
+class ApiController {
+  constructor() {
+    this.rootUrl = 'https://fe-apps.herokuapp.com/api/v1/overlook/1904';
+  }
+
+
+getAllUsers(){
+  let url = `${this.rootUrl}/users/users`;
+  return fetch(url).then(response => response.json());
+}
+
+getAllRooms() {
+  let url = `${this.rootUrl}/rooms/rooms`;
+  return fetch(url).then(response => response.json());
+}
+
+getAllBookings() {
+  let url = `${this.rootUrl}/bookings/bookings`;
+  return fetch(url).then(response => response.json());
+}
+
+
+}
+module.exports = ApiRequestController
