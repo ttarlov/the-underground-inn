54be0eafc707bb74b42e2fb20d458dfffd2bd7a6
Taras Tarlov <ttarlov@gmail.com>
2020-04-14 19:00:45 -0600

    add getCustomerById method

--- src/Customer-repo.js
+++ src/Customer-repo.js
@@ -2,5 +2,9 @@ class CustomerRepo {
   constructor(allCustomers) {
     this.customers = allCustomers;
   }
+
+  getCustomerById(id) {
+    return this.customers.find(customer => customer.id === id);
+  }
 }
 module.exports = CustomerRepo;
