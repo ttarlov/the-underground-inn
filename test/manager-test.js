2c20e4318ab81fc5685bcb3971c534d2d20c6e61
Taras Tarlov <ttarlov@gmail.com>
2020-04-15 15:07:04 -0600

    add manager test.

---
+++ test/manager-test.js
@@ -0,0 +1,20 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+import Manager from '../src/Manager';
+
+
+describe('Manager Class', function(){
+
+
+
+
+
+it.only('should be a function', function(){
+  expect(Manager).to.be.a('function')
+});
+
+
+
+
+}); // Main describe block closes here
