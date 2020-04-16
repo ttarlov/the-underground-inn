a9017259fba6447cbade0973127d14f24675551a
Taras Tarlov <ttarlov@gmail.com>
2020-04-15 20:58:04 -0600

    add test for addRoomsToBookings. Pass test.

--- test/manager-test.js
+++ test/manager-test.js
@@ -6,8 +6,64 @@ import Manager from '../src/Manager';
 
 describe('Manager Class', function(){
   let manager;
+  let rooms;
+  let bookings;
+
 beforeEach(function(){
-  manager = new Manager()
+
+  rooms = [
+    {
+    "number": 1,
+    "roomType": "residential suite",
+    "bidet": true,
+    "bedSize": "queen",
+    "numBeds": 1,
+    "costPerNight": 358.4
+    },
+    {
+    "number": 2,
+    "roomType": "suite",
+    "bidet": false,
+    "bedSize": "full",
+    "numBeds": 2,
+    "costPerNight": 477.38
+    },
+    {
+    "number": 3,
+    "roomType": "single room",
+    "bidet": false,
+    "bedSize": "king",
+    "numBeds": 1,
+    "costPerNight": 491.14
+    },
+  ];
+
+  bookings = [
+    {
+    "id": "5fwrgu4i7k55hl6sz",
+    "userID": 9,
+    "date": "2020/02/04",
+    "roomNumber": 1,
+    "roomServiceCharges": []
+    },
+    {
+    "id": "5fwrgu4i7k55hl6t5",
+    "userID": 43,
+    "date": "2020/01/24",
+    "roomNumber": 2,
+    "roomServiceCharges": []
+    },
+    {
+    "id": "5fwrgu4i7k55hl6t6",
+    "userID": 13,
+    "date": "2020/01/10",
+    "roomNumber": 3,
+    "roomServiceCharges": []
+    },
+
+  ];
+
+  manager = new Manager(bookings, rooms)
 });
 
 
@@ -19,6 +75,62 @@ it('should be a function', function(){
 
 it('should be an instance of Manager Class', function(){
   expect(manager).to.be.an.instanceof(Manager)
+});
+
+describe('addRoomsToBookings Method', function(){
+
+  it('should add matching rooms to bookings', function(){
+    expect(manager.bookings).to.deep.eq([
+  {
+    id: '5fwrgu4i7k55hl6sz',
+    userID: 9,
+    date: '2020/02/04',
+    roomNumber: 1,
+    roomServiceCharges: [],
+    bookedRoom: {
+      number: 1,
+      roomType: 'residential suite',
+      bidet: true,
+      bedSize: 'queen',
+      numBeds: 1,
+      costPerNight: 358.4
+    }
+  },
+  {
+    id: '5fwrgu4i7k55hl6t5',
+    userID: 43,
+    date: '2020/01/24',
+    roomNumber: 2,
+    roomServiceCharges: [],
+    bookedRoom: {
+      number: 2,
+      roomType: 'suite',
+      bidet: false,
+      bedSize: 'full',
+      numBeds: 2,
+      costPerNight: 477.38
+    }
+  },
+  {
+    id: '5fwrgu4i7k55hl6t6',
+    userID: 13,
+    date: '2020/01/10',
+    roomNumber: 3,
+    roomServiceCharges: [],
+    bookedRoom: {
+      number: 3,
+      roomType: 'single room',
+      bidet: false,
+      bedSize: 'king',
+      numBeds: 1,
+      costPerNight: 491.14
+    }
+  }
+])
+
+  });
+
+
 });
 
 
