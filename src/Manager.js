f2c5d9423b480d2d686437ac54726369f88a59c6
Taras Tarlov <ttarlov@gmail.com>
2020-04-15 20:57:32 -0600

    add initial properties and methods to add rooms to bookings.

--- src/Manager.js
+++ src/Manager.js
@@ -1,7 +1,26 @@
 class Manager {
-  constructor() {
+  constructor(allBookings, allRooms) {
+    this.bookings = allBookings;
+    this.roomsForToday = []
+    this.addRoomsToBookings(allRooms)
+  }
+
+  getAvailableRoomsForToday() {
+
+
 
   }
+
+
+addRoomsToBookings(allRooms) {
+  this.bookings.forEach(booking => {
+    let matchedRoom = allRooms.find(room => {
+      return room.number === booking.roomNumber
+    })
+    booking.bookedRoom = matchedRoom;
+  })
+}
+
 }
 
 export default Manager
