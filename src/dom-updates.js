2ea9f7da267e85236522d03d79c666e8fdd24e2c
Taras Tarlov <ttarlov@gmail.com>
2020-04-16 18:28:24 -0600

    add showAvailableRooms and showTotalRevenue methods.

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -10,7 +10,22 @@ addNavBar() {
   $('.nav-bar-container').prepend(`<nav>
       <div class="logo"><b>THE<span> UNDER</span>GROUND <span>INN</span></b></div>
     </nav>`).hide().show(750)
+},
+
+showAvailableRoomsTodayCard(availableRooms) {
+  $('main').prepend(`<section class= "available-rooms-card">
+      <p>Available Rooms For Today</p>
+      <p>${availableRooms}</p>
+  </section>`)
+},
+
+showTotalRevenueForToday(totalRevenue) {
+  $('main').prepend(`<section class="total-revenue-card">
+  <p>Total Revenue For Today</p>
+  <p>$${totalRevenue}</p>
+  </section>`)
 }
+
 
 }
 
