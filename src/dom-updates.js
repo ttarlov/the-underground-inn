447a94e8b9585b8785f29ef65c7f4ba15b88ec3c
Taras Tarlov <ttarlov@gmail.com>
2020-04-18 15:24:44 -0600

    add show past bookings and curent bookings methods

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -8,7 +8,12 @@ hideLoginWindow() {
 
 addNavBar() {
   $('.nav-bar-container').prepend(`<nav>
-      <div class="logo"><b>THE<span> UNDER</span>GROUND <span>INN</span></b></div>
+      <div class="nav-logo"><b>THE<span> UNDER</span>GROUND <span>INN</span></b></div>
+      <section class="nav-btns-container">
+      <button class="nav-btn" id="past-bookings">Past Room Bookings</button>
+      <button class="nav-btn" id="today-bookings">Current Room Bookings</button>
+      <button class="nav-btn" id="future-bookings">Future Room Bookings</button>
+      </section>
     </nav>`).hide().show(750)
 },
 
@@ -31,8 +36,45 @@ showPecentageOfRoomsOccupied(totalPercentage) {
   <p>Rooms Occupied Today</p>
   <p>${totalPercentage}%</p>
   </section>`)
+},
+
+showPastBookings(pastBookings) {
+  $("main").html("");
+  $("main").prepend(`<section class="bookings-container"> </section>`)
+  pastBookings.forEach(booking => {
+    $(".bookings-container").prepend(`
+      <section class="booking-cards">
+      <p>Booking Date: ${booking.date}</p>
+      <p>Room Number: ${booking.roomNumber}</p>
+      <p>Rooms Service Charges: ${booking.roomServiceCharges}
+      </section>
+      `)
+  })
+},
+
+showBookingsForToday(todaysBookings) {
+  console.log(todaysBookings);
+  if(todaysBookings.length === 0) {
+    $("main").html("");
+    $("main").prepend(`<section class="booking-cards">
+      <p class="no-rooms-found">You Have No Rooms Booked for Today</p>
+    </section>`)
+  } else {
+    $("main").html("");
+    $("main").prepend(`<section class="bookings-container"> </section>`)
+    todaysBookings.forEach(booking => {
+      $(".bookings-container").prepend(`
+        <section class="booking-cards">
+        <p>Booking Date: ${booking.date}</p>
+        <p>Room Number: ${booking.roomNumber}</p>
+        <p>Rooms Service Charges: ${booking.roomServiceCharges}
+        </section>
+        `)
+    });
+  }
 }
 
-}
+
+} //domUpdates Closes Here
 
 export default domUpdates;
