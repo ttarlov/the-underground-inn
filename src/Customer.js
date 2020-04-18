6bd2422ad220314a2e69d797a51e3f21657ab761
Taras Tarlov <ttarlov@gmail.com>
2020-04-18 16:33:18 -0600

    add domUpdates for showAmountSpentOnRooms

--- src/Customer.js
+++ src/Customer.js
@@ -16,21 +16,26 @@ class Customer {
 
   findBookingsForToday() {
     let todaysBookings = this.bookings.filter(booking => booking.date === moment().format("YYYY/MM/DD"))
-    domUpdates.showBookingsForToday(todaysBookings);
+    domUpdates.showBookingsForToday(todaysBookings); //<---TEST WITH SPIES
     return todaysBookings
   }
 
   findFutureBookings() {
-    return this.bookings.filter(booking => moment(booking.date, "YYYY/MM/DD").fromNow().includes('in'))
+    let futureBookings = this.bookings.filter(booking => moment(booking.date, "YYYY/MM/DD").fromNow().includes('in'))
+    domUpdates.showFutureBookings(futureBookings) //<-- TEST WITH SPIES
+    return futureBookings
   }
 
   calculateTotalAmountSpent() {
-    return this.bookings.reduce((totalSpend, booking) => {
+  let totalAmount =  this.bookings.reduce((totalSpend, booking) => {
 
       totalSpend += booking.bookedRoom.costPerNight
 
       return totalSpend
     },0)
+    domUpdates.showAmountSpentOnRooms(this.name, totalAmount.toFixed(2))
+  return totalAmount
+
   }
 
 
