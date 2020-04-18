8f161d00f11d2c3b69c2ee8e6c8d6cbac60a4fb3
Taras Tarlov <ttarlov@gmail.com>
2020-04-18 17:42:45 -0600

    add spy tests for all methods that trigger dom updates. pass tests.

--- test/customer-test.js
+++ test/customer-test.js
@@ -1,10 +1,13 @@
 import chai from 'chai';
 const expect = chai.expect;
+const moment = require("moment");
+const spies = require("chai-spies");
+chai.use(spies);
+
 import Customer from '../src/Customer';
 import User from '../src/User';
 import CustomerRepo from '../src/Customer-repo'
-const moment = require("moment");
-
+import domUpdates from "../src/dom-updates.js";
 
 describe('Customer Class', function(){
   let user;
@@ -15,7 +18,18 @@ describe('Customer Class', function(){
   let allBookings;
   let today;
 
+  afterEach(() => {
+    chai.spy.restore(domUpdates);
+  });
+
+
     beforeEach(function(){
+
+      chai.spy.on(domUpdates, "showPastBookings", () => {});
+      chai.spy.on(domUpdates, "showBookingsForToday", () => {});
+      chai.spy.on(domUpdates, "showFutureBookings", () => {});
+      chai.spy.on(domUpdates, "showAmountSpentOnRooms", () => {});
+
       today = moment().format("YYYY/MM/DD")
       allUsers = [
         {
@@ -222,7 +236,71 @@ describe('findPastBookings Method', function(){
       numBeds: 1,
       costPerNight: 491.14
     }
+  },
+  {
+    id: '5fwrga9i7k55hl6sz',
+    userID: 1,
+    date: '2020/04/18',
+    roomNumber: 3,
+    roomServiceCharges: [],
+    bookedRoom: {
+      number: 3,
+      roomType: 'single room',
+      bidet: false,
+      bedSize: 'king',
+      numBeds: 1,
+      costPerNight: 491.14
+    }
   }
+]
+)
+expect(domUpdates.showPastBookings).to.have.been.called(1);
+expect(domUpdates.showPastBookings).to.have.been.called.with([
+{
+id: '5fwrgu4i7k55hl6t5',
+userID: 1,
+date: '2020/01/24',
+roomNumber: 2,
+roomServiceCharges: [],
+bookedRoom: {
+  number: 2,
+  roomType: 'suite',
+  bidet: false,
+  bedSize: 'full',
+  numBeds: 2,
+  costPerNight: 477.38
+}
+},
+{
+id: '5fwrgu4i7k55hl6t6',
+userID: 1,
+date: '2020/01/10',
+roomNumber: 3,
+roomServiceCharges: [],
+bookedRoom: {
+  number: 3,
+  roomType: 'single room',
+  bidet: false,
+  bedSize: 'king',
+  numBeds: 1,
+  costPerNight: 491.14
+}
+},
+{
+id: '5fwrga9i7k55hl6sz',
+userID: 1,
+date: '2020/04/18',
+roomNumber: 3,
+roomServiceCharges: [],
+bookedRoom: {
+  number: 3,
+  roomType: 'single room',
+  bidet: false,
+  bedSize: 'king',
+  numBeds: 1,
+  costPerNight: 491.14
+}
+}
 ])
   });
 });
@@ -245,51 +323,104 @@ describe('findBookingsForToday Method', function(){
           costPerNight: 491.14
         }
       }
-    ])
+    ]);
+    expect(domUpdates.showBookingsForToday).to.have.been.called(1);
+    expect(domUpdates.showBookingsForToday).to.have.been.called.with([
+      {
+        id: '5fwrga9i7k55hl6sz',
+        userID: 1,
+        date: '2020/04/18',
+        roomNumber: 3,
+        roomServiceCharges: [],
+        bookedRoom: {
+          number: 3,
+          roomType: 'single room',
+          bidet: false,
+          bedSize: 'king',
+          numBeds: 1,
+          costPerNight: 491.14
+        }
+      }
+    ]);
   });
 });
 
 describe('findFutureBookings Method', function(){
   it('should find all future bookings', function(){
     expect(customer.findFutureBookings()).to.deep.eq([
-  {
-    id: '5fwrgu4i7k55hl6sz',
-    userID: 1,
-    date: '2022/02/04',
-    roomNumber: 1,
-    roomServiceCharges: [],
-    bookedRoom: {
-      number: 1,
-      roomType: 'residential suite',
-      bidet: true,
-      bedSize: 'queen',
-      numBeds: 1,
-      costPerNight: 358.4
-    }
-  },
-  {
-    id: '5fwrgu9i7k55hl6sz',
-    userID: 1,
-    date: '2022/02/15',
-    roomNumber: 3,
-    roomServiceCharges: [],
-    bookedRoom: {
-      number: 3,
-      roomType: 'single room',
-      bidet: false,
-      bedSize: 'king',
-      numBeds: 1,
-      costPerNight: 491.14
-    }
-  }
-])
+      {
+        id: '5fwrgu4i7k55hl6sz',
+        userID: 1,
+        date: '2022/02/04',
+        roomNumber: 1,
+        roomServiceCharges: [],
+        bookedRoom: {
+          number: 1,
+          roomType: 'residential suite',
+          bidet: true,
+          bedSize: 'queen',
+          numBeds: 1,
+          costPerNight: 358.4
+        }
+      },
+      {
+        id: '5fwrgu9i7k55hl6sz',
+        userID: 1,
+        date: '2022/02/15',
+        roomNumber: 3,
+        roomServiceCharges: [],
+        bookedRoom: {
+          number: 3,
+          roomType: 'single room',
+          bidet: false,
+          bedSize: 'king',
+          numBeds: 1,
+          costPerNight: 491.14
+        }
+      }
+    ]);
+    expect(domUpdates.showFutureBookings).to.have.been.called(1);
+    expect(domUpdates.showFutureBookings).to.have.been.called.with([
+      {
+        id: '5fwrgu4i7k55hl6sz',
+        userID: 1,
+        date: '2022/02/04',
+        roomNumber: 1,
+        roomServiceCharges: [],
+        bookedRoom: {
+          number: 1,
+          roomType: 'residential suite',
+          bidet: true,
+          bedSize: 'queen',
+          numBeds: 1,
+          costPerNight: 358.4
+        }
+      },
+      {
+        id: '5fwrgu9i7k55hl6sz',
+        userID: 1,
+        date: '2022/02/15',
+        roomNumber: 3,
+        roomServiceCharges: [],
+        bookedRoom: {
+          number: 3,
+          roomType: 'single room',
+          bidet: false,
+          bedSize: 'king',
+          numBeds: 1,
+          costPerNight: 491.14
+        }
+      }
+    ]);
   });
 
 });
 
 describe('calculateTotalAmountSpent Method', function(){
-  it.only('should be able to calculate total amount spent on rooms', function(){
-    expect(customer.calculateTotalAmountSpent()).to.eq(2309.2)
+  it('should be able to calculate total amount spent on rooms', function(){
+    expect(customer.calculateTotalAmountSpent()).to.eq("2309.20")
+    expect(domUpdates.showAmountSpentOnRooms).to.have.been.called(1)
+    expect(domUpdates.showAmountSpentOnRooms).to.have.been.called.with("Leatha Ullrich", "2309.20")
   });
 
 });
