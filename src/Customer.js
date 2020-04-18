691f1c696dadb363a1081007b2bcfe54d7c064c9
Taras Tarlov <ttarlov@gmail.com>
2020-04-18 08:45:44 -0600

    add method for finding past bookings.

--- src/Customer.js
+++ src/Customer.js
@@ -1,8 +1,16 @@
+const moment = require("moment");
 class Customer {
   constructor(customer) {
     this.id = customer.id;
     this.name = customer.name;
     this.bookings = customer.bookings;
+  }
+
+
+  findPastBookings() {
+    return this.bookings.filter(booking => {
+    return  moment(booking.date, "YYYY/MM/DD").fromNow().includes('ago')
+    })
   }
 
 }
